ef0c979b35768bb44aa39bb2d58a0776509c399a
Glitch (hello-express) <none>
2019-10-02 00:15:10 +0000

    🗺📷 Checkpoint ./views/index.html:887263/54 ./public/index.js:887263/969

--- public/index.js
+++ public/index.js
@@ -1,3 +1,30 @@
 const isServiceWorkerCapable = 'serviceWorker' in navigator;
 const isPushCapable = 'PushManager' in window;
 
+async function registerServiceWorker() {
+  console.log('TODO: Implement registerServiceWorker');
+}
+
+async function subscribeToPush() {
+  console.log('TODO: Implement subscribeToPush');
+}
+
+function getRegistration() {
+  return navigator.serviceWorker.getRegistration();
+}
+
+function getSubscription(registration) {
+  if (registration) {
+    if (registration.active) {
+      return registration.push
+    }
+  }
+}
+
+getRegistration().then(registration => { 
+  if (registration) {
+    console.log(registration.scope);
+  } else {
+    console.log('No service worker registered.');
+  }
+});
