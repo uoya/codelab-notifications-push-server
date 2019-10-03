efd0999fd1411de0a57783bcb35a3840231797c6
Glitch (hello-express) <none>
2019-10-03 01:27:14 +0000

    💆🏌 Checkpoint ./public/index.js:887263/2707

--- public/index.js
+++ public/index.js
@@ -16,71 +16,6 @@ const urlB64ToUint8Array = (base64String) => {
   return outputArray;
 };
 
-async function updateUI() {
-  let registration = await getRegistration();
-  let subscription = await getSubscription();
-  
-  let reg = document.getElementById('registration');
-  let sub = document.getElementById('subscription');
-  let regButton = document.getElementById('register');
-  let subButton = document.getElementById('subscribe');
-  let unRegButton = document.getElementById('unregister');
-  let unSubButton = document.getElementById('unsubscribe');
-  
-  reg.textContent = '';
-  sub.textContent = '';
-  regButton.disabled = true;
-  subButton.disabled = true;
-  unRegButton.disabled = true;
-  unSubButton.disabled = true;
-  
-  if (registration) {
-    reg.textContent = 
-      'Service worker registered. Scope: ' + 
-      registration.scope;
-    unRegButton.disabled = false;
-  } else {
-    reg.textContent = 'No service worker registration.'
-    regButton.disabled = false;
-  }
-  
-  if (subscription) {
-    sub.textContent = 
-      'Subscription endpoint: ' + 
-      subscription.endpoint;
-    unSubButton.disabled = false;
-  } else {
-    sub.textContent = 'No push subscription.'
-    if (registration) {
-      subButton.disabled = false;
-    }
-  }
-}
-
-function getRegistration() {
-  return navigator.serviceWorker.getRegistration();
-}
-
-async function getSubscription() {
-  let registration = await getRegistration();
-  if (!(registration && registration.active)) {
-    return false;
-  } else { 
-    return registration.pushManager.getSubscription();
-  }
-}
-
-async function registerServiceWorker() {
-  await navigator.serviceWorker.register('./serviceworker.js');
-  updateUI();
-}
-
-async function unRegisterServiceWorker() {
-  let registration = await getRegistration();
-  await registration.unregister();
-  updateUI();
-}
-
 function buildXhr(method, contentType, url) {
   let xhr = new XMLHttpRequest();
   let loadHandler = (event) => { 
@@ -102,6 +37,92 @@ function sendSubscriptionToServer(subscription) {
 function removeSubscriptionFromServer(subscription) {
   let xhr = buildXhr('POST', 'application/json', '/removesubscription');
   xhr.send(JSON.stringify(subscription));
+}
+
+async function sendNotification() {
+  let subscription = await getSubscription();
+  if (!subscription) {
+    console.log('No push subscription.');
+    return;
+  }
+  let xhr = buildXhr('POST', 'application/json', '/notify-me');
+  xhr.send(JSON.stringify({ 
+    subscription: subscription,
+    notification: {
+      title: 'Test title', 
+      options: { body: 'Test body'}
+    }
+  }));
+}
+
+async function sendNotificationToAll() {
+  let xhr = buildXhr('POST', 'application/json', '/notify-all');
+  xhr.send(JSON.stringify({ 
+    title: 'Test title', 
+    options: { body: 'Test body'}
+  }));
+}
+
+async function updateUI() {
+  let registration = await getRegistration();
+  let subscription = await getSubscription();
+  
+  let reg = document.getElementById('registration');
+  let sub = document.getElementById('subscription');
+  let regButton = document.getElementById('register');
+  let subButton = document.getElementById('subscribe');
+  let unRegButton = document.getElementById('unregister');
+  let unSubButton = document.getElementById('unsubscribe');
+  
+  reg.textContent = '';
+  sub.textContent = '';
+  regButton.disabled = true;
+  subButton.disabled = true;
+  unRegButton.disabled = true;
+  unSubButton.disabled = true;
+  
+  if (registration) {
+    reg.textContent = 
+      'Service worker registered. Scope: ' + registration.scope;
+    unRegButton.disabled = false;
+  } else {
+    reg.textContent = 'No service worker registration.'
+    regButton.disabled = false;
+  }
+  if (subscription) {
+    sub.textContent = 
+      'Subscription endpoint: ' + subscription.endpoint;
+    unSubButton.disabled = false;
+  } else {
+    sub.textContent = 'No push subscription.'
+    if (registration) {
+      subButton.disabled = false;
+    }
+  }
+}
+
+async function getRegistration() {
+  return navigator.serviceWorker.getRegistration();
+}
+
+async function getSubscription() {
+  let registration = await getRegistration();
+  if (!(registration && registration.active)) {
+    return false;
+  } else { 
+    return registration.pushManager.getSubscription();
+  }
+}
+
+async function registerServiceWorker() {
+  await navigator.serviceWorker.register('./serviceworker.js');
+  updateUI();
+}
+
+async function unRegisterServiceWorker() {
+  let registration = await getRegistration();
+  await registration.unregister();
+  updateUI();
 }
 
 async function subscribeToPush() {
@@ -127,30 +148,6 @@ async function unSubscribeFromPush() {
     await subscription.unsubscribe();
   }
   updateUI();
-}
-
-async function sendNotification() {
-  let subscription = await getSubscription();
-  if (!subscription) {
-    console.log('No push subscription.');
-    return;
-  }
-  let xhr = buildXhr('POST', 'application/json', '/notify-me');
-  xhr.send(JSON.stringify({ 
-    subscription: subscription,
-    notification: {
-      title: 'Test title', 
-      options: { body: 'Test body'}
-    }
-  }));
-}
-
-async function sendNotificationToAll() {
-  let xhr = buildXhr('POST', 'application/json', '/notify-all');
-  xhr.send(JSON.stringify({ 
-    title: 'Test title', 
-    options: { body: 'Test body'}
-  }));
 }
 
 async function initializePage() {
